a5340d927dad4d6c6a85a66bd9c091766a661557
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-26 13:12:26 +0200

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++
@@ -1,8 +0,0 @@
-# The COPYRIGHT file at the top level of this repository contains the full
-# copyright notices and license terms.
-try:
-    from trytond.modules.account_payment_wizard.tests.test_account_payment_wizard import suite
-except ImportError:
-    from .test_account_payment_wizard import suite
-
-__all__ = ['suite']
